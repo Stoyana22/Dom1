6db00cce894ecca64e33478513246b31800572b1
Stoyana22 <81923196+Stoyana22@users.noreply.github.com>
2021-04-04 20:07:28 +0300

    Create Zad1-Sum.js

---
+++ Zad1-Sum.js
@@ -0,0 +1,9 @@
+function sumNum(arr) {
+    let num1 = Number(arr[0]);
+    let num2 = Number(arr[1]);
+    let num3 = Number(arr[2]);
+
+    let sumNum = num1 + num2 + num3;
+    console.log(sumNum)
+}
+sumNum(['2', '3', '4']);
