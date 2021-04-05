eca724ca8d2d25442d9681d56d81fc37090ca7fb
Stoyana22 <81923196+Stoyana22@users.noreply.github.com>
2021-04-05 11:43:11 +0300

    Create Zad11-OddOrEven.js

---
+++ Zad11-OddOrEven.js
@@ -0,0 +1,19 @@
+function oddOrEven(number){
+	if(number % 1 == 0){
+		if(number%2 == 0){
+			console.log("even");
+
+		}else{
+			console.log("odd");
+		}
+
+
+
+	}else{
+		console.log("invalid number");
+	}
+
+}
+oddOrEven(7);
+oddOrEven(2.5);
+oddOrEven(8);
