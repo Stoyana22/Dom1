adaf4fd17b033a29591d5a78ea75f0402eb7bc85
Stoyana22 <81923196+Stoyana22@users.noreply.github.com>
2021-04-05 09:58:34 +0300

    Create Zad4-Area.js

---
+++ Zad4-Area.js
@@ -0,0 +1,14 @@
+function figureArea([w, h, W, H]){
+	let fig1Area = Number(w) * Number(h);
+	let fig2Area = Number(W) * Number(H);
+
+
+
+	let commonArea = Math.min(Number(w),Number(W)) * Math.min(Number(h),Number(H));
+
+	let figureArea = (fig1Area-commonArea)+fig2Area;
+
+	console.log("Площ: " + figureArea);
+}
+
+figureArea(['2','4','5','3'])
